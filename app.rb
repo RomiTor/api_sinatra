a292f8c1fc2b3737a9f34b26ea1982e221f00e15
Rodrigo Fuentealba Cartes <rodrigo.fuentealba.cartes@protonmail.com>
2022-05-30 23:05:41 -0400

    API funcionando

---
+++ app.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'json'
+require 'sinatra'
+require 'sinatra/json'
+
+class App < Sinatra::Application
+  post '/login' do
+    # Recibe login/pass y comprueba de que exista en tabla usuarios
+    json 'ok'
+  end
+
+  get '/error' do
+    # Recibe login/pass y comprueba de que exista en tabla usuarios
+    json 'error'
+  end
+
+  post '/post_data' do
+    #	PostData () Guarda los datos de Patente, Marca, Precio, y Usuario
+    json 'ok'
+  end
+
+  get '/get_data' do
+  	 # GetData() Obtiene desde el servidor el listado de Patente, Marca, #Precio y Usuario
+     json 'ok get data'
+  end
+
+  get '/get_marcas' do
+    # GetMarcas():Obtiene desde el servidor un listado de marcas para #desplegarlas en una lista desplegable en marca
+    json 'ok get marcas'
+  end
+
+  get '/delete/id' do
+    #	Delete (Id) :Permite Eliminar un registro en la api
+    json 'borrado'
+  end
+
+  post '/logout' do
+    # Recibe login/pass y comprueba de que exista en tabla usuarios
+    json 'ok logout'
+  end
+end
